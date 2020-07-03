f2ec7b50c4c09810a56d13e6de5060b89d864574
harishkannarao <harish.gtec@gmail.com>
2020-07-03 12:19:58 +0100

    Simplifying the tail recursion

--- application/src/main/kotlin/com/harishkannarao/kotlin/exercise/sample/MaxLetterSequence.kt
+++ application/src/main/kotlin/com/harishkannarao/kotlin/exercise/sample/MaxLetterSequence.kt
@@ -2,44 +2,34 @@ package com.harishkannarao.kotlin.exercise.sample
 
 class MaxLetterSequence {
     fun calculate(input: String): Map<Char, Int> {
-        val chars = input.toList()
-        val charWithOccurrence = calculateInternal(chars, emptyList())
-        return calculateMaxOccurrence(charWithOccurrence, emptyMap())
+        return calculateInternal(input.toList(), emptyList(), emptyMap())
     }
 
-    private tailrec fun calculateMaxOccurrence(chars: List<Pair<Char, Int>>, acc: Map<Char, Int>): Map<Char, Int> {
+    private tailrec fun calculateInternal(chars: List<Char>, occurrenceContext: List<Pair<Char, Int>>, acc: Map<Char, Int>): Map<Char, Int> {
         if (chars.isEmpty()) {
             return acc
-        }
-        val char = chars.first()
-        val remainder = chars.takeLast(chars.size - 1)
-        val currentCount = acc[char.first] ?: 0
-        return if (char.second > currentCount) {
-            calculateMaxOccurrence(remainder, acc.plus(char))
-        } else {
-            calculateMaxOccurrence(remainder, acc)
-        }
-    }
-
-    private tailrec fun calculateInternal(chars: List<Char>, occurrenceContext: List<Pair<Char, Int>>): List<Pair<Char, Int>> {
-        if (chars.isEmpty()) {
-            return occurrenceContext
         }
         val char = chars.first()
         val previousCount = occurrenceContext.lastOrNull() ?: Pair(char, 0)
         val remainder = chars.takeLast(chars.size - 1)
         return when (char) {
             ' ' -> {
-                calculateInternal(remainder, occurrenceContext)
+                calculateInternal(remainder, occurrenceContext, acc)
             }
             previousCount.first -> {
-                calculateInternal(remainder, occurrenceContext.plus(Pair(char, previousCount.second + 1)))
+                val currentOccurrence = Pair(char, previousCount.second + 1)
+                val updatedContext = occurrenceContext.plus(currentOccurrence)
+                val currentCount = acc[char] ?: 0
+                val updatedAcc = if (currentOccurrence.second > currentCount) acc.plus(currentOccurrence) else acc
+                calculateInternal(remainder, updatedContext, updatedAcc)
             }
             else -> {
-                calculateInternal(remainder, occurrenceContext.plus(Pair(char, 1)))
+                val currentOccurrence = Pair(char, 1)
+                val updatedContext = occurrenceContext.plus(currentOccurrence)
+                val currentCount = acc[char] ?: 0
+                val updatedAcc = if (currentOccurrence.second > currentCount) acc.plus(currentOccurrence) else acc
+                calculateInternal(remainder, updatedContext, updatedAcc)
             }
         }
     }
-
-
 }
