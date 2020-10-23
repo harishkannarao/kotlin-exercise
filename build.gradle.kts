52e86659c6493b2a7d4b6383c86826106a4a6dba
harishkannarao <harish.gtec@gmail.com>
2020-10-23 12:35:17 +0100

    Enabling optin requirements

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ allprojects {
 
 	tasks.withType<KotlinCompile> {
 		kotlinOptions {
-			freeCompilerArgs = listOf("-Xjsr305=strict")
+			freeCompilerArgs = listOf("-Xjsr305=strict", "-Xopt-in=kotlin.RequiresOptIn")
 			jvmTarget = javaVersion
 		}
 	}
